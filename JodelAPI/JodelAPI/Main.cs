eecf66a489e6f731d98d863377775800b9b767e1
ion <ionix@protonmail.com>
2016-06-13 08:58:34 +0200

    FIrst upload, not documented. Will be done next commit.

---
+++ JodelAPI/JodelAPI/Main.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.Web;
+using System.IO;
+
+namespace JodelAPI
+{
+    public static class Main
+    {
+        public static string accessToken = "";
+        public static string latitude = "";
+        public static string longitude = "";
+
+        private static string lastPostID = "";
+
+        public static List<Tuple<int, string, string>> GetFirstJodels()
+        {
+            string plainJson = GetPageContent("https://api.go-tellm.com/api/v2/posts/location/combo?lat=" + latitude + "&lng=" + longitude + "&access_token=" + accessToken);
+            JodelsFirstRound.RootObject jfr = JsonConvert.DeserializeObject<JodelsFirstRound.RootObject>(plainJson);
+            List<Tuple<int, string, string>> temp = new List<Tuple<int, string, string>>(); // List<counter,post_id,message>
+            int i = 0;
+            foreach(var item in jfr.recent)
+            {
+                temp.Add(new Tuple<int, string, string>(i, item.post_id, item.message));
+                i++;
+            }
+
+            lastPostID = FilterItem(temp, temp.IndexOf(temp.Last()), false); // Set the last post_id for next jodels
+
+            return temp;
+        }
+
+        public static List<Tuple<int, string, string>> GetNextJodels()
+        {
+            string plainJson = GetPageContent("https://api.go-tellm.com/api/v2/posts/location?lng="+longitude+"&lat="+latitude+"&after="+lastPostID+"&access_token="+accessToken+"&limit=1000000");
+            JodelsLastRound.RootObject jlr = JsonConvert.DeserializeObject<JodelsLastRound.RootObject>(plainJson);
+            List<Tuple<int, string, string>> temp = new List<Tuple<int, string, string>>(); // List<counter,post_id,message>
+            int i = 0;
+            foreach (var item in jlr.posts)
+            {
+                temp.Add(new Tuple<int, string, string>(i, item.post_id, item.message));
+                i++;
+            }
+
+            return temp;
+        }
+
+        public static List<Tuple<int, string, string>> GetAllJodels()
+        {
+            List<Tuple<int, string, string>> allJodels = new List<Tuple<int, string, string>>();
+            allJodels = GetFirstJodels();
+            allJodels.AddRange(GetNextJodels());
+            return allJodels;
+        }
+
+        public static string FilterItem(List<Tuple<int, string, string>> unfiltered, int index, bool filterMessage)
+        {
+            int i = unfiltered.FindIndex(t => t.Item1 == index);
+            if (!filterMessage)
+            {
+                return unfiltered[i].Item2;
+            }
+            else
+            {
+                return unfiltered[i].Item3;
+            }
+        }
+
+        private static string GetPageContent(string link)
+        {
+            WebRequest request = WebRequest.Create(link);
+            WebResponse response = request.GetResponse();
+            Stream data = response.GetResponseStream();
+            string html = String.Empty;
+            using (StreamReader sr = new StreamReader(data))
+            {
+                html = sr.ReadToEnd();
+            }
+
+            return html;
+        }
+    }
+}
---
+++ JodelAPI/JodelAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("JodelAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JodelAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("2fe543a6-f341-49d3-9cfd-98559341c69c")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
