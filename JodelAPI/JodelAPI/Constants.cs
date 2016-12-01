690af3a79c6b8f2b51c1853e847d50496c573890
ion <ionix@protonmail.com>
2016-12-01 06:48:44 +0100

    updated version

--- JodelAPI/JodelAPI/Constants.cs
+++ JodelAPI/JodelAPI/Constants.cs
@@ -7,7 +7,7 @@ namespace JodelAPI
         // Key Values
         public const string Key = "dIHNtHWOxFmoFouufSflpTKYjPmCIhWUCQHgbNzR";
         public const string ClientId = "81e8a76e-1e02-4d17-9ba0-8a7020261b26";
-        public const string AppVersion = "4.29.0";
+        public const string AppVersion = "4.29.1";
 
         // Links
         public const string LinkFirstJodels =
