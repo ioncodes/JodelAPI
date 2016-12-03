cef8304c7c233cc9bfe039460755abaa173f716e
coffeecodes <coffeecodes8@gmail.com>
2016-12-03 10:13:18 +0100

    get required location infos from google maps api by using a location string

--- JodelAPI/JodelAPI/GMaps.cs
+++ JodelAPI/JodelAPI/GMaps.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Linq;
 using System.Net;
 using System.Text;
 using JodelAPI.Json;
@@ -6,9 +7,19 @@ using Newtonsoft.Json;
 
 namespace JodelAPI
 {
-    internal static class GMaps
+    public static class GMaps
     {
-        public static string[] ToCoordinates(this string address)
+        /// <summary>
+        /// Gets a Location from an address string using Google Maps API
+        /// </summary>
+        /// <param name="address">Address search string</param>
+        /// <returns>
+        /// [0] Latitude of location
+        /// [1] Longitude of location
+        /// [2] Locality
+        /// [3] Country
+        /// </returns>
+        public static string[] GetLocationFromAddress(this string address)
         {
             string api = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address.Replace(" ", "+");
 
@@ -28,7 +39,9 @@ namespace JodelAPI
             return new[]
             {
                 gCoords.results[0].geometry.location.lat.ToString(CultureInfo.InvariantCulture),
-                gCoords.results[0].geometry.location.lng.ToString(CultureInfo.InvariantCulture)
+                gCoords.results[0].geometry.location.lng.ToString(CultureInfo.InvariantCulture),
+                gCoords.results[0].address_components.First(a => a.types[0] == "locality" && a.types[1] == "political")?.short_name,
+                gCoords.results[0].address_components.First(a => a.types[0] == "country" && a.types[1] == "political")?.short_name
             };
         }
     }
