8fecfe44e037e242987e188238a7aae403970293
ion <ionix@protonmail.com>
2016-12-07 19:10:29 +0100

    updated the version and key

--- JodelAPI/JodelAPI/Internal/Constants.cs
+++ JodelAPI/JodelAPI/Internal/Constants.cs
@@ -10,9 +10,9 @@ namespace JodelAPI.Internal
     internal class Constants
     {
         // Key Values
-        public const string Key = "VwJHzYUbPjGiXWauoVNaHoCWsaacTmnkGwNtHhjy";
+        public const string Key = "dIHNtHWOxFmoFouufSflpTKYjPmCIhWUCQHgbNzR";
         public const string ClientId = "81e8a76e-1e02-4d17-9ba0-8a7020261b26";
-        public const string AppVersion = "4.27.0";
+        public const string AppVersion = "4.29.1";
 
 
         // Headers
