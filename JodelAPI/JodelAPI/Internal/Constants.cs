05aa8715eeb7dd21bd90761586f9918e6e6c1eb3
Luca Marcelli <ionix@protonmail.com>
2017-03-09 23:03:43 +0100

    upgraded key

--- JodelAPI/JodelAPI/Internal/Constants.cs
+++ JodelAPI/JodelAPI/Internal/Constants.cs
@@ -10,9 +10,9 @@ namespace JodelAPI.Internal
     internal class Constants
     {
         // Key Values
-        public const string Key = "SDydTnTdqqaiAMfneLkqXYxamvNuUYOmkqpdiZTu";
+        public const string Key = "cYjTAwjdJyiuXAyrMhkCDiVZhshhLhotNotLiPVu";
         public const string ClientId = "81e8a76e-1e02-4d17-9ba0-8a7020261b26";
-        public const string AppVersion = "4.34.2";
+        public const string AppVersion = "4.35.6";
 
 
         // Headers
