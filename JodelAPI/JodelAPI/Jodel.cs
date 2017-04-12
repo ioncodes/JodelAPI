46e8b9566ad05060f5a84e0f5bb61d627631e59c
Luca Marcelli <ionix@protonmail.com>
2017-04-12 21:03:25 +0200

    UPDATE

--- JodelAPI/JodelAPI/Jodel.cs
+++ JodelAPI/JodelAPI/Jodel.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using JodelAPI.Internal;
 using JodelAPI.Json;
 using JodelAPI.Json.Request;
