6663711c0b4901ad3145bd4d3b500721b8a211ec
Luca Marcelli <ionix@protonmail.com>
2017-04-26 20:21:23 +0200

    updated key

--- JodelAPI/JodelAPI/Properties/AssemblyInfo.cs
+++ JodelAPI/JodelAPI/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.41.0")]
-[assembly: AssemblyFileVersion("4.41.0")]
+[assembly: AssemblyVersion("4.42.4")]
+[assembly: AssemblyFileVersion("4.42.4")]
